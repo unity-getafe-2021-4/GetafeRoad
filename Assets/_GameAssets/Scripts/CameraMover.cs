efe6c091e25e99d70878870c28bf6be2b16e4686
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-05-05 14:07:03 +0200

    Movimiento de la cámara

---
+++ Assets/_GameAssets/Scripts/CameraMover.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public float distance;
+    
+    void Update()
+    {
+        float z = transform.position.z + (distance * Time.deltaTime);
+        transform.position = 
+            new Vector3(
+                transform.position.x,
+                transform.position.y,
+                z);
+    }
+}
